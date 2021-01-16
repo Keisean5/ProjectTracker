ec73ece6d68924f79ed75cf2041c2844793e691f
Keisean5 <seanksarreal@gmail.com>
2021-01-15 23:40:50 -0800

    Model Updates

---
+++ Migrations/20210116072042_ProjectModelUpdateMigration.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectTracker.Migrations
+{
+    public partial class ProjectModelUpdateMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_Users_UserId",
+                table: "Projects");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Projects",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_Users_UserId",
+                table: "Projects",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_Users_UserId",
+                table: "Projects");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Projects",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_Users_UserId",
+                table: "Projects",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ Migrations/20210116072450_UndoProjectModelUpdateMigration.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectTracker.Migrations
+{
+    public partial class UndoProjectModelUpdateMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_Users_UserId",
+                table: "Projects");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Projects",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_Users_UserId",
+                table: "Projects",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_Users_UserId",
+                table: "Projects");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Projects",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_Users_UserId",
+                table: "Projects",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
